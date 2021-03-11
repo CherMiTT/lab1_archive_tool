f9ef0cbb582017357cd1706837000b2209bd4735
CherMiTT <chermi20000@yandex.ru>
2021-03-11 12:35:21 +0300

    Added creating the archive file

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -36,17 +39,42 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+
+//Формат файла архива: 
+//первые 8 байт - число, sizeof(структуры дескриптора для этого файла)
+//далее записывается этот дескриптор
+//далее идут файлы подряд в порядке, указанном в дескрипторе
+
+struct arch_descriptor
+{
+	int n; //число файлов
+	char **file_names; //имена файлов
+	int **file_sizes; //размеры файлов
+};
+
 void unpack(char *file_name)
 {
+	//Debug
 	printf("\nWithin unpacking function");
 }
 
 void pack(char *file_names_arr[], int length, int start_index)
 {
+	//Debug
 	printf("\nIn the packing function now.");
 	printf("\nFile names:");
 	for(int i = start_index; i < length; i++)
 	{
 		printf("\n%s", file_names_arr[i]);
+	}
+
+	//Creating archive file
+	int arch = open("test_rachive.tar", O_WRONLY | O_CREAT);
+
+
+	//Creating descriptor
+	for(int i = start_index; i < length; i++)
+	{
+
 	}
 }
