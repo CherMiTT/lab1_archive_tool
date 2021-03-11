24bdfd423730b1bb83568ea122abcaa54544b4f0
CherMiTT <chermi20000@yandex.ru>
2021-03-11 09:56:21 +0300

    Added parsing file names for pack function

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-
-void pack(char **file_names_arr);
+void pack(char *file_names_arr[], int length, int start_index);
 void unpack(char *file_name);
 
 //Программа архивирует файлы без сжатия
 
 int main(int argc, char* argv[])
 {
+	for(int i = 0; i < argc; i++)
+	{
+		printf("i = %d: %s", i, argv[i]);
+	}
+
 	//Парсим аршументы
 	int opt;
 	while ((opt = getopt(argc, argv, "pu:")) != -1)
@@ -19,14 +22,19 @@ int main(int argc, char* argv[])
 		{
 			case 'p': //pack, после него идёт список файлов для архивации
 				{
-					printf("\npacking, optind = %d\n", optind);
+					printf("\npacking, optind = %d", optind);
+					pack(argv, argc, optind);
 					break;
 				}
 			case 'u': //unpack, после него один файл - архив для распаковки
 				{
-					printf("\nunpacking, optarg = %s\n", optarg);
+					printf("\nunpacking, optarg = %s", optarg);
 					unpack(optarg);
 					break;
+				}
+			case '?':
+				{
+					printf("\nInvalig input format");
 				}
 		}
 	}
@@ -35,10 +43,15 @@ int main(int argc, char* argv[])
 
 void unpack(char *file_name)
 {
-	printf("\nWithin unpacking function\n");
+	printf("\nWithin unpacking function");
 }
 
-void pack(char **file_names_arr)
+void pack(char *file_names_arr[], int length, int start_index)
 {
-
+	printf("\nIn the packing function now.");
+	printf("\nFile names:");
+	for(int i = start_index; i < length; i++)
+	{
+		printf("\n%s", file_names_arr[i]);
+	}
 }
